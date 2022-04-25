f2ce8e3d34586d4223960a7d0d1afa6fbeb605f6
Jonas Ferreira <suporte@yesflex.com.br>
2022-04-25 16:47:00 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+abstract class Creator
+{
+    abstract public function make();
+}
+
+class Factory1 extends Creator
+{
+
+    public function make()
+    {
+        return new Concrete1();
+    }
+}
+
+class Factory2 extends Creator
+{
+
+    public function make()
+    {
+        return new Concrete2();
+    }
+}
+
+interface ConcreteInterface
+{
+    public function something();
+}
+
+class Concrete1 implements ConcreteInterface
+{
+
+    public function something()
+    {
+        return 'Concrete 1';
+    }
+}
+
+class Concrete2 implements ConcreteInterface
+{
+
+    public function something()
+    {
+        return 'Concrete 2';
+    }
+}
+
+
+$factory = new Factory1();
+$instance = $factory->make();
+var_dump($instance->something());
